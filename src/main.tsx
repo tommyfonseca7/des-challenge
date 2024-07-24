1b63efc1dff10ec5762db9887f13ff740d046e75
Tomás Ferreira Fonseca <tomasfonseca7@gmail.com>
2024-07-24 16:33:25 +0100

    Feature: Game Screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import LogIn from "./Login.tsx"
-import LoadingScreen from './LoadingScreen.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LogIn from "./Login.tsx";
+import LoadingScreen from "./LoadingScreen.tsx";
+import Game from "./Game.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LoadingScreen />
+    element: <LoadingScreen />,
   },
   {
     path: "/login",
-    element: <LogIn />
+    element: <LogIn />,
   },
   {
     path: "/game",
-    element: <App />
-  }
+    element: <Game />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
