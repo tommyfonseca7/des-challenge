d5062a97eeeb476b34a30cbac40b80618a88ee24
Tomás Ferreira Fonseca <cswsis24-tf@criticalsoftware.com>
2024-07-24 15:09:50 +0100

    Fix:Login Screen

--- src/LoadingScreen.tsx
+++ src/LoadingScreen.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ChartNoAxesCombined } from 'lucide-react';
-import './index.css'
-
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ChartNoAxesCombined } from "lucide-react";
+import "./index.css";
 
 const LoadingScreen: React.FC = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate('/login');
+      navigate("/login");
     }, 1500);
 
     return () => clearTimeout(timer);
@@ -17,9 +16,8 @@ const LoadingScreen: React.FC = () => {
 
   return (
     <div className="flex items-center justify-center w-full h-screen bg-space-cadet top-0 left-0 absolute">
-      <div className="text-center">
-        <ChartNoAxesCombined className="mx-auto text-sky-blue" size={48} />
-        <h1 className="mt-4 text-2xl font-bold text-misty-rose">Stock Market Simulator</h1>
+      <div className="flex items-center justify-center flex-col">
+        <img src=".\src\assets\image.png" alt="" className="w-1/2 h-1/2" />
         <div className="mt-4 mx-auto w-16 h-16 border-4 border-t-4 border-t-sky-blue border-space-cadet rounded-full animate-spin"></div>
       </div>
     </div>
--- src/Login.tsx
+++ src/Login.tsx
@@ -35,7 +35,7 @@ function Login() {
     if (data.username.charAt(0) === "T") {
       toast({
         title: "Please wait a few minutes",
-        description: "A game is already ocurring",
+        description: "A game is already occurring",
       });
     } else {
       toast({
@@ -47,32 +47,43 @@ function Login() {
 
   return (
     <>
-      <div className="w-full fixed top-0 left-0 bg-space-cadet shadow-md shadow-slate-gray text-teal-500 p-4">
-        <h1 className="text-xl font-bold text-white">Stock Market Simulator</h1>
-      </div>
+      <div className="flex w-full h-screen">
+        {/* Left half of the screen */}
+        <div className="w-1/2 bg-space-cadet flex items-center justify-center">
+          <img
+            src=".\src\assets\image.png"
+            alt="Logo"
+            className="w-1/2 h-auto"
+          />
+        </div>
 
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="w-full max-w-lg p-8 space-y-6 mt-20">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col space-y-2">
-                    <FormLabel className="text-left">Username</FormLabel>
-                    <div className="flex items-center space-x-4">
-                      <FormControl className="flex-grow">
-                        <Input placeholder="Username" {...field} />
-                      </FormControl>
-                      <Button type="submit">Submit</Button>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
+        {/* Right half of the screen */}
+        <div className="w-1/2 flex items-center justify-center">
+          <div className="w-full max-w-lg p-8 space-y-6">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-6"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col space-y-2">
+                      <FormLabel className="text-left">Username</FormLabel>
+                      <div className="flex items-center space-x-4">
+                        <FormControl className="flex-grow">
+                          <Input placeholder="Username" {...field} />
+                        </FormControl>
+                        <Button type="submit">Submit</Button>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </div>
         </div>
       </div>
       <Toaster />
