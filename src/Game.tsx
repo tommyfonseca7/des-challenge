c7f302c07fc2b6a6dd3d9bc7ca6f2b6c35c9ff6b
Vicente Marques <vicentemarques0709@gmail.com>
2024-07-29 09:08:28 +0100

    feat: tried transaction

--- src/Game.tsx
+++ src/Game.tsx
@@ -133,9 +133,6 @@ const GamePage = () => {
               setGameOngoingMessage(true);
             }
             break;
-          case "Transaction":
-            fetchWallet();
-            break;
           default:
             break;
         }
@@ -161,6 +158,7 @@ const GamePage = () => {
       }
       const responseData = await response.json();
       const {wallet} = responseData;
+      console.log("RESPONSE DATA: ", responseData);
       if (wallet) {
         setWallet(wallet);
       } 
@@ -226,7 +224,7 @@ const GamePage = () => {
               </h1>
             </div>
           ) : (
-            <StockTable stocks={stockData} wallet={savedWallet}/>
+            <StockTable stocks={stockData} wallet={savedWallet} ws={sendMessage} userData={userData} fetchWallet={fetchWallet}/>
           )}
         </div>
       </div>
